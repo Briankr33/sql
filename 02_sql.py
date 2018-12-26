e5626867925c3d5f507cdcd605a45ebd8547dc78
Your Name <you@example.com>
2018-12-26 08:32:10 -0600

    Add program that utilizes exception handling with SQL commands

--- 02_sql.py
+++ 02_sql.py
@@ -5,11 +5,17 @@ import sqlite3
 
 with sqlite3.connect("new.db") as connection:
 	c = connection.cursor()
-	cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8400000)")
-	cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
 
-#commit the changes
-conn.commit()
+	try:
+		c.execute("INSERT INTO populations VALUES('New York City', 'NY', 8400000)")
+		c.execute("INSERT INTO populations VALUES('San Francisco', 'CA', 800000)")
+
+		#commit the changes
+		c.commit()
+
+	except sqlite3.OperationalError:
+		print(sqlite3.OperationalError)
+
 
 # close the database connection
-conn.close()
+c.close()
